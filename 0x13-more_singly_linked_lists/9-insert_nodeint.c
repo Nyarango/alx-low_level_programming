ab5a8c76ddbf442d7f359b3faa5cc7cc2aaa73dd
Nyarango <moreennyarango0@gmail.com>
2023-08-29 10:26:16 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,56 @@
 #include "lists.h"
-
 /**
- *insert_nodeint_at_index - inserts a new node in a linked list,
- *at a given position
- *@head: pointer to the first node in the list
- *@idx: index where the new node is added
- *@n: data to insert in the new node
- *
- *Return: pointer to the new node, or NULL
- */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+ *insert_nodeint_at_index - inserts a node at a given index
+ *@idx: index to insert the node
+ *@num: value of the new node
+ *@head: pointer to the start of the list
+ *Return: pointer to the new node, NULL if failed
+ **/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int num)
 {
-unsigned int i;
-listint_t *new;
-listint_t *temp = *head;
+unsigned int count;
+listint_t *new_node, *temp;
 
-new = malloc(sizeof(listint_t));
-if (!new || !head)
+if (head == NULL)
 return (NULL);
-
-new->n = n;
-new->next = NULL;
-
 if (idx == 0)
 {
+new_node = add_nodeint(head, num);
+return (new_node);
+}
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = num;
+temp = *head;
+count = 0;
+while (count < (idx - 1))
+{
+count++;
+temp = temp->next;
+if (temp == NULL)
+return (NULL);
+}
+new_node->next = temp->next;
+temp->next = new_node;
+return (new_node);
+}
+/**
+ *add_nodeint - Adds a new node at the beginning of the list
+ *@head: pointer to the first element of the list
+ *@n: number to fill the list.
+ *Return: address of the new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new;
+
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
 new->next = *head;
+
 *head = new;
 return (new);
-}
-
-or (i = 0; temp && i < idx; i++)
-{
-if (i == idx - 1)
-{
-new->next = temp->next;
-temp->next = new;
-return (new);
-}
-else
-temp = temp->next;
-}
-
-return (NULL);
 }
